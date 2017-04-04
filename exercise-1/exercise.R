90d36ee045f366c011132649432fb6caea6c6343
Wonjo Barng <Wonjo Barng>
2017-04-04 14:58:12 -0700

    add

--- exercise-1/exercise.R
+++ exercise-1/exercise.R
@@ -1,15 +1,17 @@
 # Exercise 1: writing and executing functions
 
 # Write a function `AddThree` that adds 3 to an input value
-
-
+AddThree <- function(a) {
+  answer <- a + 3
+}
 # Create a variable `ten` by passing 7 to your `AddThree` function
-
-
+ten <- AddThree(7)
 # Write a function `FeetToMeters` that converts from feet to meters
-
-
+FeetToMeters <- function(a) {
+  answer <- a * .3048
+}
 # Create a variable `height.in.feet` that is your height in feet
-
+height.in.feet <- 6
 
 # Create a variable `height.in.meters` by passing `height.in.feet` to your `FeetToMeters` function
+height.in.meters <- FeetToMeters(height.in.feet)
